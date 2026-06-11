99b8065cc4b49a990e44b30d116ca7ea4944186f
Hathi <hathi@MacBook-Pro-de-Hathi.local>
2026-06-11 10:50:00 -0400

    Remove Cloudinary from PythonAnywhere, use local storage

--- erp/settings_pa.py
+++ erp/settings_pa.py
@@ -1,5 +1,4 @@
 from .settings import *
-from django.core.exceptions import ImproperlyConfigured
 import os
 
 DEBUG = False
@@ -10,16 +9,5 @@ CSRF_TRUSTED_ORIGINS = ['https://' + PA_DOMAIN]
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-prod-7gH8kL2mN4pQ6rS9uV1wX3yZ5')
 
-# Media files a Cloudinary (opcional, si no hay vars usa local)
-CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
-if CLOUD_NAME:
-    INSTALLED_APPS.insert(0, 'cloudinary_storage')
-    INSTALLED_APPS.append('cloudinary')
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': CLOUD_NAME,
-        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    }
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
