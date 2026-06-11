ec42d58e9ce08ba60f7c5370c4ea8fc221f90537
Hathi <hathi@MacBook-Pro-de-Hathi.local>
2026-06-11 09:28:37 -0400

    Fix settings_pa: add ImproperlyConfigured import and defaults

--- erp/settings_pa.py
+++ erp/settings_pa.py
@@ -1,22 +1,23 @@
 from .settings import *
+from django.core.exceptions import ImproperlyConfigured
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = [os.environ.get('PA_USER', '') + '.pythonanywhere.com']
+PA_USER = os.environ.get('PA_USER', 'ccespedesdevia1715')
+ALLOWED_HOSTS = [PA_USER + '.pythonanywhere.com']
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    raise ImproperlyConfigured('SECRET_KEY environment variable is required')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-prod-7gH8kL2mN4pQ6rS9uV1wX3yZ5')
 
-# Cloudinary
-INSTALLED_APPS.insert(0, 'cloudinary_storage')
-INSTALLED_APPS.append('cloudinary')
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# Media files a Cloudinary (opcional, si no hay vars usa local)
+CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+if CLOUD_NAME:
+    INSTALLED_APPS.insert(0, 'cloudinary_storage')
+    INSTALLED_APPS.append('cloudinary')
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': CLOUD_NAME,
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
