a409d48d31f900de88f4314e9e0a2a29ad8371ce
Hathi <hathi@MacBook-Pro-de-Hathi.local>
2026-07-14 20:38:42 -0400

    cleanup: remove /__deploy__/ temporary endpoint

--- erp/urls.py
+++ erp/urls.py
@@ -1,24 +1,12 @@
-import subprocess
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import render
-from django.http import HttpResponse
 from cotizaciones.views import consultar_cotizacion
 
 def home(request):
     return render(request, 'home.html')
-
-def deploy(request):
-    import os, sys
-    out = []
-    out.append("=== GIT PULL ===")
-    out.append(subprocess.check_output(["git", "pull"], cwd=os.path.dirname(os.path.dirname(__file__)), stderr=subprocess.STDOUT, timeout=30).decode())
-    out.append("=== MIGRATE ===")
-    venv_python = os.path.join(os.path.dirname(os.path.dirname(__file__)), "venv", "bin", "python")
-    out.append(subprocess.check_output([venv_python, "manage.py", "migrate"], cwd=os.path.dirname(os.path.dirname(__file__)), stderr=subprocess.STDOUT, timeout=60).decode())
-    return HttpResponse("<pre>" + "\n".join(out) + "</pre>")
 
 urlpatterns = [
     path('', home, name='home'),
@@ -27,7 +15,6 @@ urlpatterns = [
     path('portal/', include('portal.urls')),
     path('api/equipos/', include('equipos.urls')),
     path('cotizaciones/<int:numero>/consultar/', consultar_cotizacion, name='cotizacion_consultar'),
-    path('__deploy__/', deploy, name='deploy'),
 ]
 
 if settings.DEBUG:
